09522d4ca7201deef8f0df90f1fde9bbb08ebbbc
Miqueias Lima <miqueiaslima@outlook.com>
2021-04-10 15:14:39 -0300

    Tratando Erros de Rotas

--- app.js
+++ app.js
@@ -2,8 +2,30 @@ const express = require('express');
 const app = express();
 const routerProduto = require('./routes/produtos');
 const routerPedidos = require('./routes/pedidos');
+const morgan = require('morgan');
 
+app.use(morgan('dev'));
 app.use('/produtos',routerProduto);
 app.use('/pedidos',routerPedidos);
+
+
+app.use((req,res,next)=>{
+
+    const erro = new Error('Não Encontrado');
+    erro.status = 404;
+    next(erro);
+
+});
+
+app.use((error, req, res)=>{
+
+    res.status(error.status || 500);
+    return res.send({
+        erro:{
+            mensagem:error.message
+        }
+    });
+
+});
 
 module.exports = app;
